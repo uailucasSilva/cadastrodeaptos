74ba4e3e422912d348a08db5703d77622c3d3963
uailucasSilva <107223850+uailucasSilva@users.noreply.github.com>
2022-11-04 20:01:51 -0300

    Add files via upload

---
+++ src/main/java/com/aptos/cadastrodeaptos/CadastrodeaptosApplication.java
@@ -0,0 +1,13 @@
+package com.aptos.cadastrodeaptos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeaptosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeaptosApplication.class, args);
+	}
+
+}
