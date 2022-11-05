d4f4408bf8d40ef7aeb56efac748f6da303866c8
uailucasSilva <107223850+uailucasSilva@users.noreply.github.com>
2022-11-04 21:45:07 -0300

    Add files via upload
    Banco de dados configurado e telas de cadastro do apartamento e do proprietário.

--- src/main/java/com/aptos/cadastrodeaptos/controller/HomeController.java
+++ src/main/java/com/aptos/cadastrodeaptos/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.aptos.cadastrodeaptos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.aptos.cadastrodeaptos.model.Apartamento;
+import com.aptos.cadastrodeaptos.model.Proprietario;
 
 @Controller
 public class HomeController {
@@ -9,6 +13,18 @@ public class HomeController {
     @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/novo_prop")
+    public String exibeFormProp(Model model) {
+        model.addAttribute("proprietario", new Proprietario());
+        return "cad_prop";
+    }
+
+    @GetMapping("/novo_apto")
+    public String exibeFormApto(Model model) {
+        model.addAttribute("apartamento", new Apartamento());
+        return "cad_apto";
     }
 
 }
